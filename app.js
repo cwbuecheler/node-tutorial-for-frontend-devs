132c8ea95abe9db5f98e3e468a5d7aee2befd567
Christopher Buecheler <cwbuecheler@gmail.com>
2013-11-12 12:49:54 -0500

    Updating to latest edition of Express and other plugins. Updating tutorial to reflect changes.
    Signed-off-by: Christopher Buecheler <cwbuecheler@gmail.com>

--- app.js
+++ app.js
@@ -3,32 +3,27 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , mongo = require('mongodb')
-  , monk = require('monk')
-  , db = monk('localhost:27017/nodetest1');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user')
+var http = require('http');
+var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest1');
 
 var app = express();
 
-
-
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.locals.pretty = true;
 
 // development only
 if ('development' == app.get('env')) {
