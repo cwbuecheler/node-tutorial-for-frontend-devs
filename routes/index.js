cd5ffad54f20d4f82faaaf27862cf9f87183de36
Christopher Buecheler <cwbuecheler@gmail.com>
2013-12-01 14:52:15 -0500

    couple minor tweaks based on reader feedback
    Signed-off-by: Christopher Buecheler <cwbuecheler@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -58,10 +58,10 @@ exports.adduser = function(db) {
 				res.send("There was a problem adding the information to the database.");
 			}
 			else {
-				// If it worked, forward to success page 
-				res.redirect("userlist");
-				// And set the header so the address bar doesn't still say /adduser
+				// If it worked, set the header so the address bar doesn't still say /adduser
 				res.location("userlist");
+				// And forward to success page 
+				res.redirect("userlist");
 			}
 		});
 
