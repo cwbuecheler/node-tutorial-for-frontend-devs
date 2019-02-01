b04d50b8b6151a3dbba42bb5f847940648eb46bd
Christopher Buecheler <cwbuecheler@gmail.com>
2019-02-01 13:00:00 -0500

    Updated for 2019! No more Jade. Let's use EJS.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
