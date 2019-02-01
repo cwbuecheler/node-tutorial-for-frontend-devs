eeaa13d42f4ea670ba8533d9530c9fd353eb1195
Christopher Buecheler <cwbuecheler@gmail.com>
2019-02-01 13:09:10 -0500

    Updated for 2019! No more Jade. Let's use EJS.

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
-	res.render('helloworld', { title: 'Hello, World!' })
+    res.render('helloworld', { title: 'Hello, World!' });
 });
 
 /* GET Userlist page. */
@@ -50,8 +50,6 @@ router.post('/adduser', function(req, res) {
             res.send("There was a problem adding the information to the database.");
         }
         else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            //res.location("userlist");
             // And forward to success page
             res.redirect("userlist");
         }
